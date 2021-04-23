6db53970d1eae9f057171e5a65fc0c6475e98f7d
zamesow <65707749+zamesow@users.noreply.github.com>
2021-04-23 16:44:12 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import './App.module.css';
+import shortid from 'shortid';
+import PhoneBookEditor from './components/PhoneBookEditor';
+import Contacts from './components/Contacts';
+
+import './App.css';
 
 class App extends Component {
-  state = {};
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  formSubmitHandler = submitName => {
+    this.setState(prevState => ({
+      contacts: [
+        { ...submitName, id: shortid.generate() },
+        ...prevState.contacts,
+      ],
+    }));
+  };
 
   render() {
-    return <></>;
+    const { contacts } = this.state;
+
+    return (
+      <>
+        <PhoneBookEditor onSubmitProp={this.formSubmitHandler} />
+        <Contacts title="Contacts" contactsProp={contacts} />
+      </>
+    );
   }
 }
 
