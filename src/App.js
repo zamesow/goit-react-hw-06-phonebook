a427ad05442bd4d47f3282755ead575a1eb2ff80
Timur Zamesov <zamesow@gmail.com>
2021-04-23 15:59:55 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.module.css';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <></>;
+  }
+}
+
+export default App;
